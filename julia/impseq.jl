505cb0a282c322802c5f1f180dc30111ee758968
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-01-24 12:34:24 +0700

    Fix indexing mistake

--- julia/impseq.jl
+++ julia/impseq.jl
@@ -1,6 +1,7 @@
 function impseq(n0, n1::Int64, n2::Int64)
     n = collect(n1:n2)
     x = zeros(Float64,size(n)[1])
-    x[n0] = 1.0
+    idxnz = n0 - n1 + 1
+    x[idxnz] = 1.0
     return x, n
 end
